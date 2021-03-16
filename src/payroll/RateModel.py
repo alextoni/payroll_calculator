8fb807c509a11d6cc2f5aaecd9b7a16110461daf
Alejandro Perez <alextoni@gmail.com>
2021-03-16 02:02:37 -0500

    Add documentation to modules

--- src/payroll/RateModel.py
+++ src/payroll/RateModel.py
@@ -1,3 +1,9 @@
+"""
+    RateModel.py
+    This module contains the RateModel class.
+"""
+__author__ = "Alejandro Perez"
+
 import sys
 import csv
 from datetime import time
@@ -6,6 +12,11 @@ from .constants import *
 
 
 def _round_hour(t: time):
+    """
+    Rounds the time object to the nearest hour.
+    :param t: time object.
+    :return: rounded time object.
+    """
     hour = t.hour
     if t.minute > 30:
         hour = (hour + 1) % 24
@@ -13,8 +24,30 @@ def _round_hour(t: time):
 
 
 class RateModel:
+    """
+    The RateModel class represents a model for paying rates in a working week.
+    The hourly rate is based on the day of the week and the time of the day.
+
+    :ivar dict rate_map: The map containing the rate for each day and hour.
+                         For example:
+                         MO: [25, 25, 25, ... < 24 slots > ..., 20, 20, 20]
+                         TU: [25, 25, 25, ... < 24 slots > ..., 20, 20, 20]
+                         ...
+                         SU: [30, 30, 30, ... < 24 slots > ..., 25, 25, 25]
+    """
 
     def __init__(self, filename=None):
+        """
+        Creates a new rate model object.
+
+        :param filename: CSV file with the rate information (optional).
+                         Each line defines a rate for a week interval.
+                         For example:
+                         MO,00:01,09:00,25
+                         MO,09:01,18:00,15
+                         ...
+                         SU,18:01,00:00,25
+        """
         self.rate_map = {}
         for d in WEEKDAYS:
             self.rate_map[d] = [0] * DAYHOURS
@@ -22,17 +55,30 @@ class RateModel:
             self.load_model(filename)
 
     def load_model(self, filename):
+        """
+        Loads the rate model into this object.
+
+        :param filename: The CSV file path.
+        """
         try:
             csv_file = open(filename)
             csv_reader = csv.DictReader(csv_file)
             for row in csv_reader:
                 self.update_model(row['day'], row['start'], row['end'], row['rate'])
-        except FileNotFoundError as e:
+        except FileNotFoundError:
             sys.exit("ERROR: File not found: '%s'" % filename)
-        except KeyError as e:
+        except KeyError:
             sys.exit("ERROR: Bad file format: '%s'" % filename)
 
     def update_model(self, weekday, start, end, rate):
+        """
+        Updates the model with a single week interval and its rate.
+
+        :param weekday: Day of the week, in abbreviated string format.
+        :param start: Begin of interval, in time iso format.
+        :param end: End of interval, in time iso format.
+        :param rate: Rate defined for the interval, in USD.
+        """
         try:
             if weekday not in WEEKDAYS:
                 raise ValueError("'%s' is not a valid weekday" % weekday)
@@ -47,6 +93,12 @@ class RateModel:
             sys.exit("ERROR: Invalid input: " + str(e))
 
     def get_model(self, weekday):
+        """
+        Get the rate model for a single day of the week.
+
+        :param weekday: Day of the week, in abbreviated string format.
+        :return: The rate model of the day, in a list format.
+        """
         try:
             day_model = self.rate_map[weekday]
         except KeyError:
@@ -54,5 +106,8 @@ class RateModel:
         return day_model
 
     def print(self):
+        """
+        Prints the rate model in the standard output.
+        """
         for elem in self.rate_map:
             print(elem, self.rate_map[elem])
--- src/payroll/ScheduleModel.py
+++ src/payroll/ScheduleModel.py
@@ -1,3 +1,9 @@
+"""
+    ScheduleModel.py
+    This module contains the ScheduleModel class.
+"""
+__author__ = "Alejandro Perez"
+
 import sys
 import re
 from datetime import time
@@ -7,14 +13,39 @@ from .RateModel import RateModel
 
 
 def _parse_input(raw_input_line):
+    """
+    Parses the input line to extract the name and time intervals.
+    :param raw_input_line: Input string with the employee schedule info.
+    :return: The name and the list of intervals.
+    """
     name, raw_schedule_line = raw_input_line.split('=')
     intervals = re.findall("(\\D{2})(\\d{2}:\\d{2})-(\\d{2}:\\d{2})", raw_schedule_line)
     return name, intervals
 
 
 class ScheduleModel:
+    """
+    The ScheduleModel class represents the working schedule for an employee in a week.
+
+    :ivar employee_name: The employee name.
+    :ivar work_intervals: The time intervals when the employee worked. Represented
+                          by a list of tuples: (week day, start time, end time).
+    :ivar schedule_map: The map containing the week slots worked by the employee.
+                        Represented by lists of real values in [0,1] that indicate
+                        the fraction worked. For example:
+                        MO: [0, 0, 0, 0.5, 1, 1, 0.5, 0, 0, ... < 24 slots > ...]
+                        WE: [0, 0, 0, 0, 0, 1, 1, 1, 0.25, 0, ... < 24 slots > ...]
+    """
 
     def __init__(self, schedule_input=None):
+        """
+        Creates a new schedule model object.
+
+        :param schedule_input: TXT file with the working schedule information (optional).
+                               Each line represents a schedule for an employee.
+                               For example:
+                               RENE=MO10:00-12:00,TU10:00-12:00,...
+        """
         self.employee_name = None
         self.work_intervals = []
         self.schedule_map = {}
@@ -22,12 +53,24 @@ class ScheduleModel:
             self.load_schedule(schedule_input)
 
     def get_name(self):
+        """
+        Gets the employee name.
+        :return: The employee name.
+        """
         return self.employee_name
 
     def get_work_intervals(self):
+        """
+        Gets the worked time intervals in the week.
+        :return: The list of intervals, represented by tuples.
+        """
         return self.work_intervals
 
     def load_schedule(self, input_line):
+        """
+        Loads the working schedule into this object.
+        :param input_line: Input string with the employee schedule info.
+        """
         try:
             name, intervals = _parse_input(input_line)
             self.employee_name = name
@@ -52,6 +95,11 @@ class ScheduleModel:
             sys.exit("ERROR: Bad input format: " + str(e))
 
     def calculate_payment(self, rate_model: RateModel):
+        """
+        Calculates the payment amount for the employee, given a rate model to apply.
+        :param rate_model: The rate model.
+        :return: The payment amount, in USD.
+        """
         payment = 0
         for day in self.schedule_map:
             work_map = self.schedule_map[day]
@@ -61,5 +109,8 @@ class ScheduleModel:
         return payment
 
     def print(self):
+        """
+        Prints the schedule model in the standard output.
+        """
         for elem in self.schedule_map:
             print(elem, self.schedule_map[elem])
